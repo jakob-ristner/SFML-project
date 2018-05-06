dd19cac2088288791db09ddd7d6fb10e276caf7e
Vincent Udén <vincentuden@gmail.com>
2018-05-06 17:21:11 +0200

    Added main and event loop, also settings file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
-
-
+#include "./settings.cpp"
 
 int main() {
+    // Initialization of important stuff
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML Dungeon Crawler", sf::Style::Close | sf::Style::Titlebar);
+    window.setFramerateLimit(60);
+
+    sf::View viewport(sf::Vector2f((float) WINDOW_WIDTH / 2.0f, (float) WINDOW_HEIGHT / 2.0f), sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+    window.setView(viewport);
+
+    sf::Clock clock;
+
+    bool isRunning = true;
+    sf::Event event;
+
+    // Main Game Loop
+    while (isRunning) {
+        // Event Loop
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                isRunning = false;
+                window.close();
+            }
+        }
+
+        window.clear(bgColor);
+        window.display();
+    }
+
     return 0;
 }
