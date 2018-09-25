44275c1f7846315c1af5748892698fe2cdbc31e2
Jakob <jakob.ristner@itggot.se>
2018-09-25 15:40:42 +0200

    fixed resolution so it doesnt only work in i3

--- headers/Settings.h
+++ headers/Settings.h
@@ -6,8 +6,8 @@ class Settings {
 public:
     Settings();
     ~Settings();
-    const static int WINDOW_WIDTH = 1920;
-    const static int WINDOW_HEIGHT = 1080;
+    const static int WINDOW_WIDTH = 1400;
+    const static int WINDOW_HEIGHT = 800;
     const static int TIMESCALE = 10;
     const static int TILESIZE = 32;
     bool playerColliding = true;
